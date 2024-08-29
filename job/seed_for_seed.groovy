7f38fe710258f47ac59204afdbed22e9c283d419
Jason <sjason0304@gmail.com>
2024-08-30 00:48:23 +0900

    test

--- job/seed_for_seed.groovy
+++ job/seed_for_seed.groovy
@@ -2,12 +2,12 @@ job('seed_job') {
     description('This job creates other jobs and folders based on provided parameters.')
 
     parameters {
-        stringParam('FOLDER', '', '# 폴더로 구분하고자할 때만 사용 # 예시 : Test1/test2')
-        stringParam('PROJECT_NAME', 'assistant', '# 프로젝트 구분으로 Manifest Git의 폴더 구분 예시 : assistant')
-        stringParam('GIT_NAME', 'assistant-management', 'Bitbucket의 Source Git이름 예시 : assistant-management')
+        stringParam('FOLDER', '', '# 폴더로 구분하고자할 때만 사용 \n# 예시 : Test1/test2')
+        stringParam('PROJECT_NAME', 'assistant', '# 프로젝트 구분으로 Manifest Git의 폴더 구분 \n예시 : assistant')
+        stringParam('GIT_NAME', 'assistant-management', 'Bitbucket의 Source Git이름 \n예시 : assistant-management')
         choiceParam('LANG_NAME', ['java', 'python'], '# 선택 필수')
-        choiceParam('ENVIRONMENTS', ['dev', 'prod'], '# 변경 시 아래 2개 파라미터도 변경 필수 # APP_IMAGE_REGISTRY # APP_IMAGE_CREDENTIALS')
-        stringParam('APP_IMAGE_REGISTRY', 'devaiassistant.azurecr.io', '# 배포 환경에 맞는 Container Registry 주소 ## Dev devaiassistant.azurecr.io ## Prod prodaiassistant.azurecr.io')
+        choiceParam('ENVIRONMENTS', ['dev', 'prod'], '# 변경 시 아래 2개 파라미터도 변경 필수 \n# APP_IMAGE_REGISTRY \n# APP_IMAGE_CREDENTIALS')
+        stringParam('APP_IMAGE_REGISTRY', 'devaiassistant.azurecr.io', '# 배포 환경에 맞는 Container Registry 주소 \n## Dev \ndevaiassistant.azurecr.io \n## Prod \nprodaiassistant.azurecr.io')
         credentialsParam('APP_IMAGE_CREDENTIALS') {
             type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
             description('SELECTME')
