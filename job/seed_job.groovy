b7fa9b3732f4363b37273e4593486093de9ce0f3
Jason <sjason0304@gmail.com>
2024-08-30 00:18:44 +0900

    test

--- job/seed_job.groovy
+++ job/seed_job.groovy
@@ -27,7 +27,9 @@ final String param_APP_IMAGE_REGISTRY = "${APP_IMAGE_REGISTRY}"
 final String param_APP_IMAGE_CREDENTIALS = "${APP_IMAGE_CREDENTIALS}"
 // final String param_ ="${WORKSPACE}".tokenize('workspace/')
 // 이미지 생성 Job
+def jobFullName = currentBuild.rawBuild.parent.fullName
 pipelineJob("ttttt/${param_ENVIRONMENTS}-${param_GIT_NAME}") {
+    echo "$jobFullName"
     description("""
 
     """)
